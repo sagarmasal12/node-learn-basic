9e8faee380e5c16f1525be2f7894cc6599674c62
sagarmasal12 <sagarmasal12@gmail.com>
2025-08-12 10:17:55 +0530

    try dsa code

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { log } = require("console");
 var fs = require("fs");
 
 fs.writeFileSync("sagar.txt","My Name is Sagar")
@@ -11,5 +12,29 @@ fs.writeFileSync("sagar.txt","My Name is Sagar")
 
 // fruit('Apple')
 
+// var myArr = [7, 12, 9, 4, 11];
+// var minVal = myArr[0];
+
+// for(i in myArr){
+//     if (i < minVal){
+//         minVal = i
+//     }
+
+// }
+// console.log(minVal)
+
+
+var myArr = [7, 12, 9, 4, 11,2,55,23];
+var minVal = myArr[0];
+console.log(minVal);
+
+
+for (let num of myArr) {
+    if (num < minVal) {
+        minVal = num;
+    }
+}
+
+console.log(minVal); // 4
 
 
