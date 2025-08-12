559c50c452007b3479496416bb5ddcabfffc1daa
sagarmasal12 <sagarmasal12@gmail.com>
2025-08-12 10:03:45 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var fs = require("fs");
+
+fs.writeFileSync("sagar.txt","My Name is Sagar")
+
+// console.log(20)
+// console.log('hello node',20+20);
+
+// function fruit(item){
+//     console.log(item)
+// }
+
+// fruit('Apple')
+
+
+
