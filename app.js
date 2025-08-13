5868f20d257535360c9a3d1fc6e317419b667ef9
sagarmasal12 <sagarmasal12@gmail.com>
2025-08-13 14:59:35 +0530

    all practice code

--- app.js
+++ app.js
@@ -3,66 +3,66 @@ const { log } = require("console");
 var fs = require('fs')
 
 fs.writeFileSync("sagar.txt","My Name is Sagar")
-// fs.writeFileSync("math.js",'');
+fs.writeFileSync("math.js",'');
 
-// console.log(20)
-// console.log('hello node',20+20);
+console.log(20)
+console.log('hello node',20+20);
 
-// function fruit(item){
-//     console.log(item)
-// }
+function fruit(item){
+    console.log(item)
+}
 
-// fruit('Apple')
+fruit('Apple')
 
-// var myArr = [7, 12, 9, 4, 11];
-// var minVal = myArr[0];
+var myArr = [7, 12, 9, 4, 11];
+var minVal = myArr[0];
 
-// for(i in myArr){
-//     if (i < minVal){
-//         minVal = i
-//     }
+for(i in myArr){
+    if (i < minVal){
+        minVal = i
+    }
 
-// }
-// console.log(minVal)
-
-
-// var myArr = [7, 12, 9, 4, 11,2,55,23];
-// var minVal = myArr[0];
-// console.log(minVal);
+}
+console.log(minVal)
 
 
-// for (let num of myArr) {
-//     if (num < minVal) {
-//         minVal = num;
-//     }
-// }
-
-// console.log(minVal); // 4
+var myArr = [7, 12, 9, 4, 11,2,55,23];
+var minVal = myArr[0];
+console.log(minVal);
 
 
-// const math = require('./math')
-// console.log("Addition:", math.add(5, 3));       // Expected: 8
-// console.log("Multiplication:", math.multiply(4, 2)); // Expected: 8
+for (let num of myArr) {
+    if (num < minVal) {
+        minVal = num;
+    }
+}
+
+console.log(minVal); // 4
 
 
-// var a= 300;
-// let b = 200;
-// let c = 50
+const math = require('./math')
+console.log("Addition:", math.add(5, 3));       // Expected: 8
+console.log("Multiplication:", math.multiply(4, 2)); // Expected: 8
 
-// console.log(a+b+c)
 
-// if(a==300){
-//     // debugger
-//     console.log('this is if codnition');
+var a= 300;
+let b = 200;
+let c = 50
+
+console.log(a+b+c)
+
+if(a==300){
+    // debugger
+    console.log('this is if codnition');
     
-// }else{
+}else{
     
-//     console.log("this is else condition")
-// }
+    console.log("this is else condition")
+}
 
-// for(let i=1; i<=10; i++){
-//     console.log(i)
-// }
+for(let i=1; i<=10; i++){
+    console.log(i)
+}
 
 
 var users = ["Anil","Sam", "Pratik","Joel"];
